b85db475c9cf7af285fd99b12e1986a443265a78
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-02-17 16:16:37 -0500

    creates file hello-world

--- node-server.js
+++ node-server.js
@@ -3,10 +3,14 @@ var fs =  require('file-system');
 const port = 3000;
 
 const createFile = (request) => {
-    fs.writeFileSync('hello-world.txt', 'Hello to this greaat world!', 'utf8');
+    fs.writeFile('hello-world.txt', 'Hello to this greaat world!', 'utf8', (err) => {
+        if(err) throw err;
+
+        console.log('success');
+    });
 };
 
-const server = http.createServer(createFile);
+const server = http.createServer(createFile());
 
 server.listen(port, (err) => {
     if(err){
